a448afc44d27e22d5ad62bb69db8011ccf9d4ba2
Vishal Jangir <jangir02vishal@gmail.com>
2023-10-18 22:33:37 +0530

    import error

--- Bookstore-client/src/dashboard/DashboardLayout.jsx
+++ Bookstore-client/src/dashboard/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React  from "react";
 import { Outlet } from "react-router-dom";
-import SideBar from "/SideBar";
+import SideBar from "./Sidebar";
 
 const DashboardLayout = () =>{
     return (
